a91b8d9627943fc48b109756e8ee4bc2d1d4bbca
zeyou-commits <zeboudji.youssef@gmail.com>
2026-09-21 12:23:51 +0200

    feat: wire trip planner selection to map

--- src/components/panel/SidePanel.tsx
+++ src/components/panel/SidePanel.tsx
@@ -53,7 +53,7 @@ export function SidePanel({
             <div className="h-2 bg-secondary/40" />
           </div>
         ) : null}
-        <TripPlanner />
+        <TripPlanner onSelect={onSelect} />
         <ExplorerView
           filters={filters}
           onFiltersChange={onFiltersChange}
